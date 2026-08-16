3c937c2c54a5003c18f7c63acb7a2aa1531e5925
apt2736 <apt2736@gmail.com>
2026-08-16 14:30:45 +0700

    replacing tinymlgen with m2cgen

--- triage_pipeline.h
+++ triage_pipeline.h
@@ -1,11 +1,8 @@
 #ifndef TRIAGE_PIPELINE_H
 #define TRIAGE_PIPELINE_H
-
 #ifdef __cplusplus
 extern "C" {
 #endif
-
-// Raw Input Features (15 Values)
 typedef struct {
     float age;
     float cc_breathingdifficulty;
@@ -23,12 +20,10 @@ typedef struct {
     float spo2_max;
     float sbp_max;
 } TriageInput;
-// 5-Class Output Probabilities and Predicted ESI Level
 typedef struct {
-    float probs[5]; // Index 0..4 maps to ESI 1..5
-    int predicted_esi; // 1..5
+    float probs[5];
+    int predicted_esi;
 } TriageOutput;
-// Primary C Pipeline Entry Point
 TriageOutput predict_triage(const TriageInput* input);
 #ifdef __cplusplus
 }
